b407a47f5da726782ca40f1ab9e2438460a43154
yinyo7 <gideonnaftaly@gmail.com>
2022-01-25 14:50:11 +0700

    Sistem Jadwal Training

---
+++ app/Views/admin/beranda.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h1 class="text-blue"><b><marquee>Selamat Datang Admin Cabin Maintenance Service (TN)</marquee></b></h1>
+        </div>    
+    </div><center>
+    <div class="col-sm-12">
+        <img class="img-responsive pad" width="150%" src="<?= base_url()?>/gambar/beranda.jpeg" alt="Photo">
+    </div></center>
+</div>
---
+++ app/Views/layout/v_nav.php
@@ -0,0 +1,58 @@
+<!-- Collect the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav text-bold">
+
+            
+            <li><a href="<?= base_url(); ?>/admin">Dashboard</a></li>
+            <li><a href="<?= base_url(); ?>/admin/dashboard">Training</a></li>            
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+
+                    
+            <!-- User Account Menu -->
+            <li class="dropdown user user-menu">
+              <!-- Menu Toggle Button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <!-- The user image in the navbar-->
+                <img src="<?= base_url('foto/'. session()->get('avatar')); ?>" width="20px" height="20px" class="img-circle" alt="User Image">&nbsp;
+                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                <span class="hidden-xs text-bold" ><?= session()->get('nama'); ?></span>
+              </a>
+              <ul class="dropdown-menu">
+                <!-- The user image in the menu -->
+                <li class="user-header">
+                  <img src="<?= base_url('foto/'. session()->get('avatar')); ?>" class="img-circle" alt="User Image">
+
+                  <p class="text-bold text-blue">
+                    <?= session()->get('email'); ?>
+                  </p>
+                </li>
+                <!-- Menu Footer-->
+                <li class="user-footer">
+                  <div class="pull-left">
+                    <a href="<?= base_url('profile'); ?>" class="btn btn-default btn-flat">Profile</a>
+                  </div>
+                  <div class="pull-right">
+                    <a href="<?= base_url('auth/logout'); ?>" class="btn btn-default btn-flat">Sign out</a>
+                  </div>
+                </li>
+              </ul>
+            </li>
+                      </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      
+      <!-- Main content -->
+      <section class="content">
